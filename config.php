841249cbe15fd3f87b9219b42aaa032d856ab8ff
callyjoe <calvinakiweley54@gmail.com>
2026-09-19 18:25:33 -0700

    Move API key and DB credentials out of version control

--- config.php
+++ config.php
@@ -1,13 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "galorem_auth_new";
+require_once __DIR__ . '/secrets.php';
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-define('MISTRAL_API_KEY', 'COE44lUR8WR95rvozEaRl8JkSR9ocT6U');
-// Check connection
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
---
+++ secrets.example.php
@@ -0,0 +1,6 @@
+<?php
+define('MISTRAL_API_KEY', 'your-mistral-api-key');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'galorem_auth_new');
